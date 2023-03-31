e8243ed6397fb03c54768316d9b0c5a7969857de
Cathybiwott <biwottcathy@gmail.com>
2023-03-31 19:56:37 +0300

    first commit

---
+++ math.py
@@ -0,0 +1,20 @@
+def add (a,b):
+    answer = a+b
+    return answer
+
+def subtract (c,d):
+     answer1= c-d
+     return answer1
+
+def divide (f,g):
+    answer2 = f/g
+    return answer2
+
+def multiply (z,m):
+    answer3 = z*m
+    return answer3
+
+def remainder (f,n):
+    answer4 = f%n
+    return answer4
+    
